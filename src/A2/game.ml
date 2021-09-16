998c57297af85d79275c1e7d3294620537bb7ae2
Wayne Deng <wayne28@michaels.com>
2021-09-16 18:04:41 +0800

    update game

--- src/A2/game.ml
+++ src/A2/game.ml
@@ -1,17 +1,95 @@
-let get_game_attribute attribute : Yojson.Basic.t -> Yojson.Basic.t =
-  let rec get_game_attribute_iter :
-      (string * Yojson.Basic.t) list -> Yojson.Basic.t = function
-    | [] -> failwith "empty file"
-    | h :: t ->
-      match h with
-      | k, v -> if k = attribute then v else get_game_attribute_iter t
-  in
-  function
-  | `Assoc tl -> get_game_attribute_iter tl
-  | _ -> failwith "wrong format"
+#use "topfind"
 
-let get_win_message json =
-  let m = get_game_attribute "win_message" json in
-  match m with
-  | `String s -> s
-  | _ -> failwith "no win_message in file"
+#require "yojson"
+
+open Yojson.Basic.Util
+
+type description =
+  { requires : string list
+  ; text : string
+  }
+
+type exit =
+  { direction : string
+  ; room_id : string
+  ; keys : string list
+  }
+
+type room =
+  { id : string
+  ; descriptions : description list
+  ; point : int
+  ; exits : exit list
+  ; treasure : string list
+  }
+
+type item =
+  { id : string
+  ; description : string
+  ; points : int
+  }
+
+type start_location =
+  { room : string
+  ; item : string
+  }
+
+type game =
+  { roooms : room list
+  ; start_room : string
+  ; items : item list
+  ; start_inv : string list
+  ; start_locations : start_location list
+  ; win_message : string
+  }
+
+let rooms json =
+  let room j =
+    let id = j |> member "id" |> to_string in
+    let descriptions =
+      let description =
+        let requires =
+          d |> member "requires" |> to_list |> List.map to_string
+        in
+        let text = d |> member "text" |> to_string in
+        { requires; text }
+      in
+      j |> member "descriptions" |> to_list |> List.map description
+    in
+    let point = j |> member "point" |> to_string in
+    let exits =
+      let exit =
+        let direction = es |> member "direction" |> to_string in
+        let room_id = es |> member "room_id" |> to_string in
+        let keys = es |> member "keys" |> to_list |> List.map to_string in
+        { direction; room_id; keys }
+      in
+      j |> member "exits" |> to_list |> List.map exit
+    in
+    let treasure = j |> member "treasure" |> to_list |> List.map to_string in
+    { id; descriptions; point; exits; treasure }
+  in
+  json |> member "rooms" |> to_list |> List.map room
+
+let start_room json = json |> member "start_room" |> to_string
+
+let items json =
+  let item j =
+    let id = j |> member "id" |> to_string in
+    let description = j |> member "description" |> to_string in
+    let points = j |> member "points" |> to_int in
+    { id; description; points }
+  in
+  json |> member "items" |> to_list |> List.map item
+
+let start_inv json = json |> member "start_inv" |> to_list |> List.map to_string
+
+let start_locations json =
+  let start_location j =
+    let room = j |> member "room" |> to_string in
+    let item = j |> member "item" |> to_string in
+    { room; item }
+  in
+  json |> member "start_locations" |> to_list |> List.map start_location
+
+let win_message json = json |> member "win_message" |> to_string
